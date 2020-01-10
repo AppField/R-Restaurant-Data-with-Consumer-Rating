11a751006208cc314e2ebf6b69b485a4f5129bae
an0101st <a.stanusic.as@gmail.com>
2020-01-10 13:20:00 +0100

    API Deployment added

---
+++ API_Deployment/client_test.R
@@ -0,0 +1,10 @@
+library(httr)
+library(rjson)
+test = data.frame(x = c(1, 99, 500))
+
+str = toJSON(test)
+content(POST("http://127.0.0.1:8080/regression", body = str, encode = "json"))
+
+content(POST("http://127.0.0.1:8080/regression", body = "bla", encode = "json"))
+
+
