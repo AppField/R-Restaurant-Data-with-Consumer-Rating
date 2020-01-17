2cdd5253306cec4202967883273e15ad1c573bd1
an0101st <a.stanusic.as@gmail.com>
2020-01-17 14:19:32 +0100

    Dashboaard overview started

--- customer_rating_dashboard/app.R
+++ customer_rating_dashboard/app.R
@@ -24,53 +24,65 @@ sidebar <- dashboardSidebar(
     sidebarMenu(
         menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
         menuItem( "Daten", tabName = "data", icon = icon("list-alt",lib='glyphicon'),
+                  menuSubItem("Overview", tabName = "data_overview", icon = icon("blackboard",lib='glyphicon')),
                   menuSubItem("Restaurant", tabName = "restaurant", icon = icon("cutlery",lib='glyphicon')),
                   menuSubItem("User", tabName = "user", icon = icon("user",lib='glyphicon')),
                   menuSubItem("Rating", tabName = "rating", icon = icon("equalizer",lib='glyphicon'))),
         menuItem( "Machine Learning", tabName = "ml", icon = icon("random",lib='glyphicon'),
+                  menuSubItem("Overview", tabName = "pred_overview", icon = icon("blackboard",lib='glyphicon')),
                   menuSubItem("Predicting Rating", tabName = "pred_rating", icon = icon("equalizer",lib='glyphicon')),
                   menuSubItem("Predicting Cuisine", tabName = "pred_cuisine", icon = icon("cutlery",lib='glyphicon')))
     )
-)
-
-
-frow1 <- fluidRow(
-    valueBoxOutput("value1")
-    ,valueBoxOutput("value2")
-    ,valueBoxOutput("value3")
-)
-
-frow2 <- fluidRow(
-    
-    box(
-        title = "Revenue per Account"
-        ,status = "primary"
-        ,solidHeader = TRUE 
-        ,collapsible = TRUE 
-        ,plotOutput("revenuebyPrd", height = "300px")
-    )
-    
-    ,box(
-        title = "Revenue per Product"
-        ,status = "primary"
-        ,solidHeader = TRUE 
-        ,collapsible = TRUE 
-        ,plotOutput("revenuebyRegion", height = "300px")
-    ) 
-    
 )
 
 #BODY
 body <- dashboardBody(
     tabItems(
         tabItem(tabName = "dashboard",
-                h2("Dashboard tab content")
+                h2("Dashboard Customer - Rating"),
+                fluidRow(
+                    valueBoxOutput("datasets"),
+                    valueBoxOutput("dataset_rows"),
+                    valueBoxOutput("ml_method"),
+                ),
+                fluidRow(
+                    box(
+                        title = "Beschreibung",
+                        #status = "primary",
+                        solidHeader = TRUE,
+                        collapsible = TRUE, 
+                        "In dem Projekt wurden Daten von Restaurant Bewertungen ausgewerten und im
+                        späterer Folge ein Model zu entwickelt, um dies vorherzusagen. <br>
+                        Neben der Vorhersage der Bewertung (Rating) wurde ein Model erstellt,
+                        um die Küche vorherzusagen, welche ein Kunde mit bestimmten Attributen
+                        besuchen würde."
+                    ),
+                    tabBox(
+                        title = "First tabBox",
+                        # The id lets us use input$tabset1 on the server to find the current tab
+                        id = "tabset1", height = "250px",
+                        tabPanel("Tab1", "First tab content"),
+                        tabPanel("Tab2", "Tab content 2")
+                    ),
+                ),
         ),
-        tabItem(tabName = "data",
-                h2("Data tab content")
+        tabItem(tabName = "data_overview",
+                  h2("Data tab content"),
+                  box(
+                      title = "Inputs", 
+                      status = "primary",
+                      sliderInput("slider", "Slider input:", 1, 100, 50),
+                      textInput("text", "Text input:")
+                  )
         ),
         tabItem(tabName = "restaurant",
-                h2("restaurant tab content")
+                h2("Data tab content"),
+                box(
+                    title = "Inputs", 
+                    status = "primary",
+                    sliderInput("slider", "Slider input:", 1, 100, 50),
+                    textInput("text", "Text input:")
+                )
         ),
         tabItem(tabName = "user",
                 h2("user tab content")
@@ -78,8 +90,8 @@ body <- dashboardBody(
         tabItem(tabName = "rating",
                 h2("rating tab content")
         ),
-        tabItem(tabName = "ml",
-                h2("ML tab content")
+        tabItem(tabName = "pred_overview",
+                h2("rating pred tab content")
         ),
         tabItem(tabName = "pred_rating",
                 h2("rating pred tab content")
