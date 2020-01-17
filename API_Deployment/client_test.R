7f4c76af6ee834df059dbb8f987cf7e7718bc953
an0101st <a.stanusic.as@gmail.com>
2020-01-17 13:17:31 +0100

    Implemented client_test cases

--- API_Deployment/client_test.R
+++ API_Deployment/client_test.R
@@ -1,10 +1,12 @@
 library(httr)
 library(rjson)
-test = data.frame(x = c(1, 99, 500))
+test = data.frame(smoker = c(TRUE, FALSE, FALSE), 
+                  drink_level = c("casual drinker", "abstemious", "social drinker"),
+                  budget = c("medium", "low", "medium"),
+                  birth_year = c(1950, 1990, 1985),
+                  Rcuisin = c("Asian", "Persian", "European"))
 
 str = toJSON(test)
-content(POST("http://127.0.0.1:8080/regression", body = str, encode = "json"))
-
-content(POST("http://127.0.0.1:8080/regression", body = "bla", encode = "json"))
+content(POST("http://127.0.0.1:8080/rf", body = str, encode = "json"))
 
 
